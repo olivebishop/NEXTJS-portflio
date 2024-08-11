a942ffbe47d4d260daed7e2c258a9896a49f7d65
olivebishop <olivehendrilgen1@gmail.com>
2024-08-11 16:52:22 +0300

    fix:changes on OG image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -101,7 +101,7 @@ export default async function Image() {
                             textShadow: '1px 1px 2px rgba(0,0,0,0.3)',
                         }}
                     >
-                        Creative Developer & Digital Innovator
+                        Software Developer &  Chess Player
                     </p>
                     <div
                         style={{
