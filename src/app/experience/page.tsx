d59b329c0eca51a39f8f5b02f0c96c5d489f9861
olivebishop <olivehendrilgen1@gmail.com>
2024-08-15 01:24:47 +0300

    fix: some fixes on bg images on al pages

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { experiences } from "@/utils"
+import { MdMailOutline } from "react-icons/md";
 
 interface Experience {
   date: string;
@@ -12,7 +13,7 @@ interface Experience {
 
 const ExperienceItem: React.FC<Experience> = ({ date, title, company, description, tasks }) => {
   return (
-    <div className="flex flex-col items-center text-center">
+    <div className="flex flex-col items-center text-center overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] dark:before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element-dark.svg')] before:bg-no-repeat before:bg-top before:bg-cover before:size-full before:-z-[1] before:transform before:-translate-x-1/2">
       <div className="mb-2">
         <h3 className="text-sm font-medium text-gray-600 dark:text-neutral-400">
           {date}
@@ -43,28 +44,25 @@ const ExperienceItem: React.FC<Experience> = ({ date, title, company, descriptio
 
 const FreelanceProject: React.FC = () => {
   return (
-    <div className="mt-16 p-6 bg-gray-100 dark:bg-gray-800 rounded-lg shadow-md text-center">
+    <div className="mt-16 p-6 bg-gray-100 dark:bg-[#373737] rounded-lg shadow-md text-center">
       <h3 className="text-xl font-semibold text-gray-800 dark:text-neutral-200 mb-4">
         Recent Freelance Project
       </h3>
       <p className="text-md text-gray-600 dark:text-neutral-400 mb-4">
-        Developed a tours and travel website featuring:
+      <span className="mr-2 text-green-500">•</span>
+      The Sol of African Tours and Travel
       </p>
       <ul className="space-y-2">
-        {[
-          "SEO optimization",
-          "Analytics integration",
-          "OG image generation",
-          "Responsive design",
-          "Enhanced UX/UI",
-          "Built with Next.js, TypeScript, Tailwind CSS, and Framer Motion"
-        ].map((feature, index) => (
-          <li key={index} className="flex items-center justify-center">
-            <span className="mr-2 text-blue-500">✓</span>
-            <span className="text-sm text-gray-600 dark:text-neutral-400">{feature}</span>
-          </li>
-        ))}
-      </ul>
+  <li className="flex flex-col sm:flex-row justify-center items-center gap-4">
+    <a href="/contact" className="rounded-md bg-gray-900 text-white px-3 py-2 text-center w-full sm:w-auto">
+      Get a Quote
+    </a>
+    <a href="https://the-sol-of-african-tours.vercel.app/" target="_blank" rel="noopener noreferrer" className="rounded-md bg-gray-900 text-white px-3 py-2 text-center w-full sm:w-auto">
+      View Demo
+    </a>
+  </li>
+</ul>
+
     </div>
   );
 };
